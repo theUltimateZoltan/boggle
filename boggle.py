3e1ca0d9da55b10dcb48fc859ddc969222a9b5ab
Elad Levy <elad.levy2@mail.huji.ac.il>
2020-01-19 13:34:40 +0200

    progress with boggle.py some changes to config

--- boggle.py
+++ boggle.py
@@ -1,7 +1,8 @@
 
 from board import Board
 from screen import Screen
-from boggle_config import GameConfig
+from boggle_config import GameConfig as Cfg
+from threading import Timer
 import sys
 
 
@@ -9,12 +10,23 @@ class GameRunner:
     def __init__(self):
         self.__board = Board()
         self.__score = 0
-        self.__time = 0
+        self.__timer = Timer(1.0, self.every_second)
+        self.__remaining_time = Cfg.game_time
         self.__correct_words = set()
         self.__wrong_words = set()
 
     def run(self):
-        pass
+        """
+        Start running the game with default settings.
+        :return: None
+        """
+        self.__timer.start()
+
+    def every_second(self):
+        self.__remaining_time -= 1
+        # screen - change timer printed
+        if self.__remaining_time <= 0:
+            self.times_up()
 
     def reset(self):
         pass
@@ -22,20 +34,34 @@ class GameRunner:
     def exit(self):
         pass
 
-    def set_score(self):
-        pass
+    def add_score(self, score):
+        # screen - print "well done" message
+        self.__score += score
 
-    def add_word(self):
-        pass
+    def add_word(self, word):
+        """
+        Check if the word is valid.
+        if valid and not yet marked, add as correct.
+        if invalid and not yet marked - ad ass wrong.
+        if already marked - display informative message.
+        :param word: The word to check and add
+        :return: None
+        """
+        if word in self.__correct_words or word in self.__wrong_words:
+            # screen - display informative message.
+            return
+        # TODO : convert boggle_dict to a set of valid words for comparison
 
     def buy_time(self):
-        pass
+        self.__score -= Cfg.Buy_time_price
+        self.__remaining_time += Cfg.Buy_time_seconds
 
     def add_letter(self):
         pass
 
     def times_up(self):
-        pass
+        self.__timer.cancel()
+        # screen - show end game message
 
 
 if __name__ == "__name__":
--- boggle_config.py
+++ boggle_config.py
@@ -1,3 +1,6 @@
 
 class GameConfig:
-    pass
+    Buy_time_price = 10
+    Buy_time_seconds = 10
+
+    game_time = 180  # seconds
