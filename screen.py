cc73eebe9a9796255a7543d298166d34b28d787d
Elad Levy <elad.levy2@mail.huji.ac.il>
2020-01-19 16:35:44 +0200

    progress with screen.py changed import statement

--- screen.py
+++ screen.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+from tkinter import *
 
 # frames, widgets, button, canvas, entry, label, menu, text, scroll bar, Listbox
 
@@ -8,13 +8,27 @@ class Screen:
         self._init_graphics()
 
     def _init_graphics(self):
-        self._root = tk.Tk()
+        self._root = Tk()
         self._root.title("Boggle Game!")
         self._root.config(bg="white")
+        self._root.resizable(0, 0)  # don't allow resizing
         # TODO - add icon
 
+    def build_lists(self):
+        """
+        Build graphics for correct\wrong lists initial state
+        :return: tuple (correct-list, wrong-list)
+        """
+        correct = Text(self._root, height=530, width=120, fg="#1ABCB4", font="Segoe ui")
+        wrong = Text(self._root, height=530, width=120, bg="#FF6D6D", font="Segoe ui")
+        correct.pack()
+        wrong.pack()
+        correct_title = Text(correct, height=30, bg="#1ABCB4", font="Segoe ui", fg="#FFF")
+        wrong_title = Text(wrong, height=30, bg="#1ABCB4", font="Segoe ui", fg="#FFF")
+        correct_title.pack(fill=X)
+
     def start_screen(self):
-        tk.mainloop()
+        mainloop()
 
     def end_game(self):
         """
