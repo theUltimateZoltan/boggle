84723303f976f283ff824e7bf4b06c3c7f2ff85f
Ron Shuvy <ronshuvy@users.noreply.github.com>
2020-01-19 17:33:29 +0200

    Here we go again..

--- screen.py
+++ screen.py
@@ -1,6 +1,9 @@
-import tkinter as tk
+from tkinter import *
+from PIL import ImageTk, Image
 
 # frames, widgets, button, canvas, entry, label, menu, text, scroll bar, Listbox
+# Red : #FF6D6D
+# Turquoise : #1ABCB4
 
 
 class Screen:
@@ -8,13 +11,27 @@ class Screen:
         self._init_graphics()
 
     def _init_graphics(self):
-        self._root = tk.Tk()
+        self._root = Tk()
+        self._root.geometry("1100x700")
         self._root.title("Boggle Game!")
         self._root.config(bg="white")
-        # TODO - add icon
+        self._root.resizable(0, 0)
+
+        f1 = Frame(self._root)
+        f2 = Frame(self._root)
+        f3 = Frame(self._root)
+        f1.pack(side=LEFT)
+        f2.pack(side=LEFT)
+        f3.pack(side=LEFT)
+        button1 = Button(f1, text="Push me", fg="red")
+        button2 = Button(f2, text="Push me", fg="blue")
+        button3 = Button(f3, text="Push me", fg="green")
+        button1.pack()
+        button2.pack()
+        button3.pack()
 
     def start_screen(self):
-        tk.mainloop()
+        mainloop()
 
     def end_game(self):
         """
